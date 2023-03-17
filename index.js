e38f6d49722e476268bfc358f885922825db8700
Alphabitic <alphabitic@gmail.com>
2023-03-17 16:39:54 +0100

    first commit

--- index.js
+++ index.js
@@ -7,8 +7,30 @@ import moment from 'moment';
 import 'moment/locale/fr.js';
 import axios from "axios";
 
+const urls= [
+  "https://webmail.buyin.pro/lm_auth_proxy?DoLMLogin?curl=L2fowa&curlid=3446992831-3598899218",
+  "https://mail.francemutuelle.fr/lm_auth_proxy?DoLMLogin?curl=L2fowa&curlid=661763043-2620574924",
+  "https://webmail.adapei65.fr/ ",
+  "https://webmail.francebrevets.com",
+  "https://sts.lixir.fr/",
+  "https://mail.nidek.fr/owa",
+  "https://webmail.bredinprat.com/lm_auth_proxy?DoLMLogin?curl=L2fowa&curlid=1831998750-2022892364&curlmode=0",
+  "https://www.lycra.com/en/coolmax-business",
+  "https://www.fo-rothschild.fr",
+  "https://francemutuelle.neocles.com",
+  "https://adapei65.neocles.com",
+  "https://gieccifinance.neocles.com",
+  "https://gieccifinance.neocles.ir",
+  "https://envoludia.neocles.com",
+  "https://eri.neocles.com/vpn/index_2auth.html",
+  "https://proudreed.neocles.com/vpn/index.html",
+  "https://procie.neocles.com/vpn/index.html",
+  "https://sfcdc65.neocles.com",
+  "https://sagess-ctx.neocles.com",
+  ];
+
 // Configuration de CORS
-app.use(cors());
+const app = express();
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://zola-testurl.netlify.app"); // remplacer "monsite.com" par le nom de domaine réel du site web
@@ -23,13 +45,13 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
-
+app.use(cors());
 moment.locale('fr');
 const port=process.env.PORT;
 // Utilisation de Moment
 const today = moment().locale('fr').format('LL');
 dotenv.config();
-const app = express();
+
 app.use(express.json())
 // Middleware pour le parsing du corps de la requête
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -43,7 +65,7 @@ app.get('/check-url', async (req, res) => {
     const url = req.query.url;
   
     try {
-      const response = await fetch(url, { method: 'HEAD' });
+      const response = await fetch(url, { method: 'GET' });
       const status = response.status;
       res.json({ url, status });
     } catch (error) {

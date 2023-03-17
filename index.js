725213ecef7f4d77718dc7058a3c656a707a419f
Alphabitic <alphabitic@gmail.com>
2023-03-17 16:03:18 +0100

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,29 @@ import moment from 'moment';
 import 'moment/locale/fr.js';
 import axios from "axios";
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin",   "https://webmail.buyin.pro/lm_auth_proxy?DoLMLogin?curl=L2fowa&curlid=3446992831-3598899218",
+  "https://mail.francemutuelle.fr/lm_auth_proxy?DoLMLogin?curl=L2fowa&curlid=661763043-2620574924",
+  "https://webmail.adapei65.fr/ ",
+  "https://webmail.francebrevets.com",
+  "https://sts.lixir.fr/",
+  "https://mail.nidek.fr/owa",
+  "https://webmail.bredinprat.com/lm_auth_proxy?DoLMLogin?curl=L2fowa&curlid=1831998750-2022892364&curlmode=0",
+  "https://www.lycra.com/en/coolmax-business",
+  "https://www.fo-rothschild.fr",
+  "https://francemutuelle.neocles.com",
+  "https://adapei65.neocles.com",
+  "https://gieccifinance.neocles.com",
+  "https://gieccifinance.neocles.ir",
+  "https://envoludia.neocles.com",
+  "https://eri.neocles.com/vpn/index_2auth.html",
+  "https://proudreed.neocles.com/vpn/index.html",
+  "https://procie.neocles.com/vpn/index.html",
+  "https://sfcdc65.neocles.com",
+  "https://sagess-ctx.neocles.com",); // remplacer "monsite.com" par le nom de domaine réel du site web
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 moment.locale('fr');
 const port=process.env.PORT;
 // Utilisation de Moment
